e3fb01b9a303a6cfede6b4cfcd1068a91f49c8af
nakamura-ta <57085424+nakamuratanakamurata@users.noreply.github.com>
2023-02-07 20:08:33 +0900

    Update modelnet10_classify.py

--- modelnet10_classify.py
+++ modelnet10_classify.py
@@ -1,3 +1,28 @@
+from pathlib import Path
+
+from torch_geometric.datasets import ModelNet
+import torch_geometric.transforms as T
+
+current_path = Path.cwd()
+dataset_dir = current_path / "modelnet10"
+
+pre_transform = T.Compose([
+    T.SamplePoints(1024, remove_faces=True, include_normals=True),
+    T.NormalizeScale(),
+])
+
+train_dataset = ModelNet(dataset_dir, name="10", train=True, transform=None, pre_transform=pre_transform, pre_filter=None)
+test_dataset = ModelNet(dataset_dir, name="10", train=False, transform=None, pre_transform=pre_transform, pre_filter=None)
+
+
+
+from torch_geometric.data import DataLoader as DataLoader
+dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
+batch = next(iter(dataloader))
+print(batch)
+
+
+
 from torch_geometric.nn import global_max_pool
 import torch.nn as nn
 
